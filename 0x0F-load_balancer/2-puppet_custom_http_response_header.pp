d9b64dd1c0732e9305c0f384295f4cff1dbff229
matt969696 <matsce2020@gmail.com>
2021-04-12 11:14:54 +0000

    feat() : add initial solutions

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -8,11 +8,16 @@ package { 'nginx':
   require => Exec['update'],
 }
 
+file { '/var/www/html/index.html':
+  content => 'Holberton School',
+  require => Package['nginx'],
+}
+
 file_line { 'redirect':
   ensure  => 'present',
   path    => '/etc/nginx/sites-available/default',
   after   => 'listen 80 default_server;',
-  line    => 'rewrite ^/redirect_me https://bayona.me permanent;',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
   require => Package['nginx'],
 }
 
@@ -21,11 +26,6 @@ file_line { 'addHeader':
   path    => '/etc/nginx/sites-available/default',
   after   => 'listen 80 default_server;',
   line    => 'add_header X-Served-By $hostname;',
-  require => Package['nginx'],
-}
-
-file { '/var/www/html/index.html':
-  content => 'Holberton School',
   require => Package['nginx'],
 }
 
