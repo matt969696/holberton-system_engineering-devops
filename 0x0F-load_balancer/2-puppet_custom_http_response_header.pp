c413d8ffd1e5e0d72bc6e611a4c0083e4985a086
matt969696 <matsce2020@gmail.com>
2021-04-12 11:07:22 +0000

    feat() : add initial solutions

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,28 +1,35 @@
 # Tentative to write a correct Puppet manifest
+exec { 'update':
+  command => '/usr/bin/apt-get update',
+}
 
 package {'nginx':
-  ensure => present,
-  name   => 'nginx',
+  ensure  => installed,
+  name    => 'nginx',
+  require => Exec['update'],
 }
 
 file {'/var/www/html/index.html':
-  ensure  => present,
+  ensure  => 'present',
   path    => '/var/www/html/index.html',
   content => 'Holberton School\n',
+  require => Package['nginx']
 }
 
 file_line { 'redirect_me':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  require => Package['nginx']
 }
 
 file_line { 'add_header':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'add_header X-Served-By $hostname;',
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'add_header X-Served-By $hostname;',
+  require => Package['nginx']
 }
 
 service { 'nginx':
@@ -30,4 +37,5 @@ service { 'nginx':
   enable     => true,
   hasrestart => true,
   require    => Package['nginx'],
+  subscribe  => [File_line['redirect_me'], File_line['add_header'],
 }
