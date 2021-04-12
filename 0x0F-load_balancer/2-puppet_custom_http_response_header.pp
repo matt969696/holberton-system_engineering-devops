ec4d6e940e06e0fa710df1c7842e865c00b1985a
matt969696 <matsce2020@gmail.com>
2021-04-12 10:01:51 +0000

    feat() : add initial solutions

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,35 @@
+# Tentative to write a correct Puppet manifest
+
+package {'nginx':
+  ensure => present,
+  name   => 'nginx',
+}
+
+file {'/var/www/html/index.html':
+  ensure  => present,
+  path    => '/var/www/html/index.html',
+  content => 'Holberton School\n',
+}
+
+file_line { 'redirect_me':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+file_line { 'add_header':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'add_header X-Served-By $hostname;',
+}
+
+service { 'nginx':
+  ensure     => running,
+  enable     => true,
+  hasrestart => true,
+  require    => Package['nginx'],
+  subscribe  => File_line['redirect_me'],
+  subscribe  => File_line['add_header'],
+}
