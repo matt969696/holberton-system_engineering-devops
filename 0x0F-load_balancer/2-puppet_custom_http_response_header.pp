a3d03836f9756fdcc1a86aa5ca62452ad2f9cdf2
matt969696 <matsce2020@gmail.com>
2021-04-12 11:12:22 +0000

    feat() : add initial solutions

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,37 +3,33 @@ exec { 'update':
   command => '/usr/bin/apt-get update',
 }
 
-package {'nginx':
+package { 'nginx':
   ensure  => installed,
-  name    => 'nginx',
   require => Exec['update'],
 }
 
-file {'/var/www/html/index.html':
-  ensure  => 'present',
-  path    => '/var/www/html/index.html',
-  content => 'Holberton School',
-  require => Package['nginx']
-}
-
-file_line { 'redirect_me':
+file_line { 'redirect':
   ensure  => 'present',
   path    => '/etc/nginx/sites-available/default',
   after   => 'listen 80 default_server;',
-  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-  require => Package['nginx']
+  line    => 'rewrite ^/redirect_me https://bayona.me permanent;',
+  require => Package['nginx'],
 }
 
-file_line { 'add_header':
+file_line { 'addHeader':
   ensure  => 'present',
   path    => '/etc/nginx/sites-available/default',
   after   => 'listen 80 default_server;',
   line    => 'add_header X-Served-By $hostname;',
-  require => Package['nginx']
+  require => Package['nginx'],
+}
+
+file { '/var/www/html/index.html':
+  content => 'Holberton School',
+  require => Package['nginx'],
 }
 
 service { 'nginx':
-  ensure     => running,
-  require    => Package['nginx'],
-  subscribe  => [File_line['redirect_me'], File_line['add_header'],
+  ensure  => running,
+  require => Package['nginx'],
 }
