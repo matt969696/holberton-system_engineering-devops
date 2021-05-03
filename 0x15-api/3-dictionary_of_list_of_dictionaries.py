c85e5819d782754f23157ec7c727c46f0e081839
matt969696 <matsce2020@gmail.com>
2021-05-03 15:29:05 +0000

    fix() : removed one API call

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -8,11 +8,12 @@ if __name__ == "__main__":
     users = requests.get(url + "users").json()
 
     with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump({
-            u.get("id"): [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": u.get("username")
+        for u in users :
+            json.dump({
+                u.get("id"): [{
+                    "task": t.get("title"),
+                    "completed": t.get("completed"),
+                    "username": u.get("username")
             } for t in requests.get(url + "todos",
                                     params={"userId": u.get("id")}).json()]
-            for u in users}, jsonfile)
+            }, jsonfile)
