e65033ed12aa0a5a66fc48b2063c48516e1d6fdf
matt969696 <matsce2020@gmail.com>
2021-05-03 15:25:50 +0000

    fix() : removed one API call

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,30 +1,38 @@
 #!/usr/bin/python3
-"""
-Module that, using this REST API, for a given employee ID,
-returns information about his/her TODO list progress
-"""
+"""Gather data from an API"""
+import csv
 import json
 import requests
-import sys
+from sys import argv
 
 
 def main():
-    """ Returns employee information TODO """
-    site = "https://jsonplaceholder.typicode.com/users/"
-    listemp = requests.get(site).json()
-    myDict = {}
-    with open("todo_all_employees.json", 'w') as outfile:
-        for employee in listemp:
-            taskl = requests.get(site + str(employee["id"]) + "/todos").json()
-            mylist = []
+    """main function"""
 
-            for task in taskl:
-                taskdic = {"task": task['title'],
-                           "completed": task['completed'],
-                           "username": employee["username"]}
-                mylist.append(taskdic)
+    usersUrl = "https://jsonplaceholder.typicode.com/users/"
 
-            json.dump({employee["id"]: mylist}, outfile)
+    usersReq = requests.get(usersUrl)
+    users = json.loads(usersReq.text)
+
+    masterDict = {}
+    for user in users:
+        todoUrl = ("https://jsonplaceholder.typicode.com/"
+                   "users/{:s}/todos".format(str(user['id'])))
+        todoReq = requests.get(todoUrl)
+        todo = json.loads(todoReq.text)
+
+        taskList = []
+        for task in todo:
+            formattedTask = {"task": task['title'],
+                             "completed": task['completed'],
+                             "username": user['username']}
+            # print(formattedTask)
+            taskList.append(formattedTask)
+        masterDict.update({user['id']: taskList})
+
+    masterJSON = json.dumps(masterDict)
+    with open("todo_all_employees.json", "w") as file:
+        file.write(masterJSON)
 
 
 if __name__ == "__main__":
