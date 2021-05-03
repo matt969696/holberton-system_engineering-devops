d46ab1d8c157b2ef45bc0dbe257922ecb52cdf7f
matt969696 <matsce2020@gmail.com>
2021-05-03 14:53:15 +0000

    fix() : changed comment

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
+"""
+Module that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress
+"""
 import requests
 import sys
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+def main():
+    """ Returns employee information TODO """
+    site = "https://jsonplaceholder.typicode.com/users/"
+    empl = requests.get(site + sys.argv[1]).json()['name']
+
+    json = requests.get(site + sys.argv[1] + "/todos").json()
+    nb = len(json)
+    c = sum(task.get('completed') is True for task in json)
+    print("Employee {} is done with tasks({}/{}):".format(empl, c, nb))
+
+    for task in json:
+        if task.get('completed') is True:
+            print("\t {}".format(task.get('title')))
+
+
+if __name__ == "__main__":
+    main()
